0f9d1aa79a48ac18bd27e569ae28ae3071cab852
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-02-02 23:17:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function add7(num) {
+  return num + 7;
+}
 
+function multiply(a, b) {
+  return a * b;
+}
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.substring(1).toLowerCase();
+}
+
+function lastLetter(str) {
+  return str[str.length - 1]
+}
+
+console.log(add7(4))
+console.log(multiply(24, 8))
+console.log(capitalize('HeLLo, WoRLd!'))
+console.log(lastLetter('abcd'))
